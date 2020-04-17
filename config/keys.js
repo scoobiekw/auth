d9e26f8cf5d92c7b4d8cf319632f335f8c2d3c82
Steve Kennedy-Williams <scoobiekw@gmail.com>
2020-04-17 11:22:07 -0700

    bar

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://scoobiekw:jerrygarciaisnotandycoe@cluster0-uxvuq.mongodb.net/mern-auth?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
